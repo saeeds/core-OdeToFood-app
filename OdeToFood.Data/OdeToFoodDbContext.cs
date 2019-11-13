2fbb5fbd2892139ce1cb1c077ea29bdc94a7ad70
Saeed Alsharqawi <saed_sharqawi@hotmail.com>
2019-11-13 22:25:33 +0300

    Adding Entity Framework

---
+++ OdeToFood.Data/OdeToFoodDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public class OdeToFoodDbContext : DbContext
+    {
+        public OdeToFoodDbContext(DbContextOptions<OdeToFoodDbContext> options)
+                  : base(options)
+        {
+
+        }
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
